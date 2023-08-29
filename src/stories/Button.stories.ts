a063c5726015a9dbe26269822085f362e40b9b41
Yan Yashyn <yan.yashyn.s@gmail.com>
2023-08-29 14:39:31 +0300

    Updated Storybook Docs

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -7,6 +7,7 @@ const meta = {
   component: Button,
   parameters: {
     layout: "centered",
+    componentSubtitle: "Displays a Button component",
   },
   tags: ["autodocs"],
 } satisfies Meta<typeof Button>;
@@ -16,8 +17,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    label: "Button",
-    children: "Get Started"
+    children: "Get Started",
   },
 };
-
